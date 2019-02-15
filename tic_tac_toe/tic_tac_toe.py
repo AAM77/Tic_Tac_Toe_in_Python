d1fcc03d003ce67e453e92c30bbb1a9b7bb62660
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-14 22:09:53 -0500

    Added two variables for player 1 & 2. Added a method that asks user input to choode X or O. Added a separate method that sets player 1 and 2 to be what the input is.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,3 +1,7 @@
+
+player_1 = ''
+player_2 = ''
+
 # GREETS THE PLAYER
 def greet_player():
     print("Welcome to Tic-Tac-Toe")
@@ -28,4 +32,31 @@ def print_game_logo():
     print(title)
 
 
-    
+    # ASKS THE PLAYER TO CHOOSE X or O
+def choose_player():
+
+    player_choice = input("Do you want to be 'X' or 'O'?").upper()
+
+    if player_choice == 'X':
+        print("You are now 'X', Player One.")
+        print("Player two is 'O'.")
+        print("Starting the game.")
+    elif player_choice == 'O':
+        print("You are now 'O', Player One.")
+        print("Player two is 'X'.")
+        print("Starting the game.")
+    else:
+        print("Incorrect choice. Choose 'X' or 'O'")
+        choose_player()
+
+    set_players(player_choice)
+
+
+# SETS THE PLAYERS_1 and PLAYER_2 to
+def set_players(player_choice):
+    if player_choice.upper() == 'X':
+        player_1 = 'X'
+        player_2 = 'O'
+    else:
+        player_1 = 'O'
+        player_2 = 'X'
