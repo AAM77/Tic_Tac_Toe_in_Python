9bcb6c2135ce11efec911779a85d7d9feee87f7c
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-14 22:05:04 -0500

    Added two methods: greet_player and print_game_logo method

---
+++ tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,31 @@
+# GREETS THE PLAYER
+def greet_player():
+    print("Welcome to Tic-Tac-Toe")
+
+# PRINTS THE GAME LOGO
+def print_game_logo():
+
+    title = """
+    #######  #######   ######
+       #        #      #
+       #        #      #
+       #        #      #
+       #     #######   ######
+
+    #######   ####     ######
+       #     #    #    #
+       #     ######    #
+       #     #    #    #
+       #     #    #    ######
+
+    #######   ####     ######
+       #     #    #    #
+       #     #    #    ######
+       #     #    #    #
+       #      ####     ######
+    """
+
+    print(title)
+
+
+    
