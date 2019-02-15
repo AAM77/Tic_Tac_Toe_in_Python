2f0609969ace98a6c8fbf86bcfa184356ba6d846
AAM77 <Adeel.A.Mhd@gmail.com>
2019-02-14 23:18:42 -0500

    Tested the functions in jupyter notebook. Corrected issues. Added the test file here. Updated the code in the main .py file

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,7 +1,6 @@
 
 board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-player_1 = ''
-player_2 = ''
+players = ["",""]
 
 # GREETS THE PLAYER
 def greet_player():
@@ -34,7 +33,7 @@ def print_game_logo():
 
 
     # ASKS THE PLAYER TO CHOOSE X or O
-def choose_player():
+def choose_x_or_o():
 
     player_choice = input("Do you want to be 'X' or 'O'?").upper()
 
@@ -48,7 +47,7 @@ def choose_player():
         print("Starting the game.")
     else:
         print("Incorrect choice. Choose 'X' or 'O'")
-        choose_player()
+        choose_x_or_o()
 
     set_players(player_choice)
 
@@ -56,11 +55,11 @@ def choose_player():
 # SETS THE PLAYERS_1 and PLAYER_2 to
 def set_players(player_choice):
     if player_choice.upper() == 'X':
-        player_1 = 'X'
-        player_2 = 'O'
+        players[0] = 'X'
+        players[1] = 'O'
     else:
-        player_1 = 'O'
-        player_2 = 'X'
+        player[0] = 'O'
+        player[1] = 'X'
 
 
 # PRINTS THE BOARD
@@ -75,13 +74,14 @@ def print_board():
 
 # GETS USER INPUT AND DETERMINES IF VALID
 def pick_position():
-    desired_position = input("Choose a position to place your mark. (1-9)")
+    desired_position = int(input("Choose a position to place your mark. (1-9)"))
 
-    if ((0 <= player_position) and (player_position <= 9)):
-        update_board(desired_position)
+    if ((1 <= desired_position) and (desired_position <= 9)):
+        board_index = desired_position - 1
+        update_board(board_index)
     else:
         print("Invalid choice. You must choose a position from 1 - 9")
-        choose_position()
+        pick_position()
 
 # UPDATES THE BOARD ARRAY WITH THE NEW POSITION
 def update_board(pos_choice):
